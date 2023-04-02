ae31126a3a2f3c37fae117dad8eee0408c3af918
Triani Putri Mumpuni <127008970+trianipm@users.noreply.github.com>
2023-04-03 00:34:13 +0700

    menambahkan class KuliahMahasiswa1302213091

---
+++ KuliahMahasiswa1302213091.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MOD7_KPL_1302213091_TRIANI_PUTRI_MUMPUNI_AFF.KuliahMahasiswa1302213091
+{
+    internal class KuliahMahasiswa1302213091
+    {
+        public List<MataKuliah1302213091> courses { get; set; }
+
+        public KuliahMahasiswa1302213091(List<MataKuliah1302213091> matkul)
+        {
+            this.courses = matkul;
+        }
+        public KuliahMahasiswa1302213091()
+        {
+        }
+    }
+    public class MataKuliah1302213091
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+
+        public MataKuliah1302213091(string kode, string nama)
+        {
+            code = kode;
+            name = nama;
+        }
+        public MataKuliah1302213091() { }
+
+    }
+}
