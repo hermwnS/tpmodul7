551f16bc773741b76fc49361c3c8a3ad86296e53
Bangley123 <yesasihombing1@gmail.com>
2023-04-02 22:16:23 +0700

    menambahkan class Kuliah Mahasiswa

--- tpmodul7_kelompok_04/tpmodul7_kelompok_04/KuliahMahasiswa_1302213108.cs
+++ tpmodul7_kelompok_04/tpmodul7_kelompok_04/KuliahMahasiswa_1302213108.cs
@@ -8,5 +8,27 @@ namespace tpmodul7_kelompok_04
 {
     internal class KuliahMahasiswa_1302213108
     {
+        public List<MataKuliah_1302213108> courses { get; set; }
+
+        public KuliahMahasiswa_1302213108(List<MataKuliah_1302213108> matkul)
+        {
+            this.courses = matkul;
+        }
+        public KuliahMahasiswa_1302213108()
+        {
+        }
+    }
+    public class MataKuliah_1302213108
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+
+        public MataKuliah_1302213108(string kode, string nama)
+        {
+            code = kode;
+            name = nama;
+        }
+        public MataKuliah_1302213108() { }
+
     }
 }
--- tpmodul7_kelompok_04/tpmodul7_kelompok_04/Program.cs
+++ tpmodul7_kelompok_04/tpmodul7_kelompok_04/Program.cs
@@ -12,6 +12,23 @@ internal class Program
         String jsonString = File.ReadAllText("D:\\Folder kpl\\tpmodul7\\tpmodul7_kelompok_04\\tpmodul7_kelompok_04\\bin\\Debug\\net6.0\\tp7_1_1302213108.json");
         data = JsonSerializer.Deserialize<DataMahasiswa_1302213108>(jsonString);
         Console.WriteLine("Nama " + data.nama.depan + " " + data.nama.belakang + " dengan nim " + data.nim + " dari fakultas " + data.fakultas);
+
+        String jsonString2 = File.ReadAllText("D:\\Folder kpl\\tpmodul7\\tpmodul7_kelompok_04\\tpmodul7_kelompok_04\\bin\\Debug\\net6.0\\tp7_2_1302213108.json");
+        //Dezerialize 
+        KuliahMahasiswa_1302213108 kuliah = JsonSerializer.Deserialize<KuliahMahasiswa_1302213108>(jsonString2);
+        if (kuliah.courses != null)
+        {
+            int i = 1;
+            foreach (MataKuliah_1302213108 matkul in kuliah.courses)
+            {
+                Console.WriteLine("MK"+(i) + " " +  matkul.code+" __ " + matkul.name);
+                i++;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Kosong");
+        }
     }
 
 }
