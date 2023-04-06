8ed839fb85c31c55e03120b3d448fda4c876e09b
hermwnS <126874286+hermwnS@users.noreply.github.com>
2023-04-06 22:45:33 +0700

    Create KuliahMahasiswa_1302213080.cs
    menambahkan class KuliahMahasiswa_130221080

---
+++ tpmodul7_kelompok_04/tpmodul7_kelompok_04/KuliahMahasiswa_1302213080.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_04
+{
+    internal class KuliahMahasiswa_1302213080
+    {
+        public List<MataKuliah_1302213080> courses { get; set; }
+
+        public KuliahMahasiswa_1302213080(List<MataKuliah_1302213080> matkul)
+        {
+            this.courses = matkul;
+        }
+        public KuliahMahasiswa_1302213080()
+        {
+        }
+    }
+    public class MataKuliah_1302213080
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+
+        public MataKuliah_1302213080(string kode, string nama)
+        {
+            code = kode;
+            name = nama;
+        }
+        public MataKuliah_1302213080() { }
+
+    }
+}
